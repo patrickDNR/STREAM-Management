8e294835ee7c806b2e76f4bbb07da3f7babf1eb0
Patrick Kelly <patrick.kelly@wisconsin.gov>
2026-02-25 15:10:54 -0600

    Added all Q and Water qual data

--- Scripts/DataWrangling.R
+++ Scripts/DataWrangling.R
@@ -1,3 +1,11 @@
 #Script to Pull Q and water qual data to merge files and wrangle some data
 
 #HEllo World
+
+library(lubridate)
+library(tidyverse)
+library(leaflet)
+
+
+#Open metadata to get all SpC and NO3
+meta <- read.csv('Data/')
